741a88f1c15592bfe79454209f3a61d8f8bb1aa3
ABardot <aabardot5@gmail.com>
2018-03-09 11:25:22 -0500

    Add my Category Class and Review Class into my model

---
+++ MVCReviewProject/Models/Review.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int GameID { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string GameReview { get; set; }
+        public int GameScore { get; set; }
+        public double Price { get; set; }
+
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+    }
+}
