2874a6d752929b5f47bd71f49d202a0917dde6b6
ABardot <aabardot5@gmail.com>
2018-03-09 13:07:30 -0500

    Added Display names to my fields and cleaned up some code

--- MVCReviewProject/Models/Category.cs
+++ MVCReviewProject/Models/Category.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCReviewProject.Models
 {
     public class Category
     {
-        public int Id { get; set; }
+        [Key]
+        public int CategoryID { get; set; }
+
         public string Name { get; set; }
         public virtual ICollection<Review> Reviews { get; set; }
     }
--- MVCReviewProject/Models/Review.cs
+++ MVCReviewProject/Models/Review.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace MVCReviewProject.Models
 {
@@ -11,19 +8,29 @@ namespace MVCReviewProject.Models
     {
         [Key]
         public int GameID { get; set; }
+
+        [Display(Name = "Game Title")]
         public string Title { get; set; }
+
+        [Display(Name = "Publisher")]
         public string Publisher { get; set; }
+
+        [Display(Name = "Date Released")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime PublishedDate { get; set; }
+
+        [Display(Name = "Game Review")]
         public string GameReview { get; set; }
+
+        [Display(Name = "Game Rating")]
         public int GameScore { get; set; }
+
+        [Display(Name = "Price")]
         public double Price { get; set; }
 
-
-        [ForeignKey("Category")]
+        [ForeignKey("Category"), Display(Name = "Category")]
         public int CategoryID { get; set; }
+
         public virtual Category Category { get; set; }
-
-
-
     }
 }
